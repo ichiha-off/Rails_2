33387b11961f61b17d392376ea5708820f0f0b08
ichiha-off <bsk.6510riku@gmail.com>
2020-05-30 12:12:40 +0900

    Bookers_Rails2

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = 'b23b137699449683d56a06ffa3c4ba53c70a0a0c12268790fb0ad3a4148a817d78e997994013e871c6c9fb8b882b93f2dbb61afc30be4701b9d6f0c39d66aee4'
